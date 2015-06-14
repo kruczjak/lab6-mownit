6c7bb87be14af1da1cf456e91d9e6d28d9a12313
kruczjak <kruczjak@gmail.com>
2015-06-14 05:09:08 +0200

    Initial commit

---
+++ BagOfWords.py
@@ -0,0 +1,116 @@
+from scipy.sparse import lil_matrix, csr_matrix
+from scipy.sparse.linalg import svds as svd
+from numpy import diag
+import numpy as numpy
+import time
+
+__author__ = 'kruczjak'
+
+bag_of_words = {}
+texts = []
+
+def update_progress(progress, max, time):
+    print '\r[{0}] {1}/{2} time: {3}ms'.format('#'*(progress/10), progress, max, time*1000)
+
+def prepare(text):
+    return text.replace(",", "").replace(".", "").replace(" - ", " ").replace("\'", " ").lower().split()
+
+
+text_count = int(raw_input("Il. plikow tekstowych: "))
+query = raw_input("Wyraz: ")
+k = int(raw_input("Ilosc wynikow: "))
+
+main_start_time = start_time = time.time()
+print "Rozpoczynam czytanie"
+for i in range(text_count):
+    file = open("texts/texts/"+str(i+1)+".txt")
+    texts.append(set(prepare(file.read())))
+    file.close()
+    update_progress(i, text_count, time.time() - start_time)
+
+# 2, 3
+start_time = time.time()
+print "Tworzenie bag of words"
+for i in range(text_count):
+    for word in texts[i]:
+        if word not in bag_of_words.keys():
+            bag_of_words[word] = len(bag_of_words)
+    update_progress(i, text_count, time.time() - start_time)
+
+N = len(bag_of_words)
+T = len(texts)
+
+# 4
+start_time = time.time()
+print "Tworzenie term-by-document matrix"
+term_by_document = lil_matrix((N, T))
+for i in range(text_count):
+    for word in texts[i]:
+        term_by_document[bag_of_words[word], i] += 1
+    update_progress(i, text_count, time.time() - start_time)
+
+# N = len(bag_of_words)
+# T = len(texts)
+
+# 5
+start_time = time.time()
+print "Obliczanie inverse document frequency"
+idf = [0]*N
+for i in range(N):
+    idf[i] = term_by_document[i, :].getnnz()
+    idf[i] = numpy.log(T/idf[i])
+    update_progress(i, N, time.time() - start_time)
+
+# przemnazanie przez IDF
+start_time = time.time()
+print "Normalization with IDF"
+for i in range(N):
+    for j in range(T):
+        term_by_document[i, j] *= idf[i]
+    update_progress(i, N, time.time() - start_time)
+
+# 8
+start_time = time.time()
+print "Low-rank approximation with SVD"
+(U, S, V) = svd(term_by_document, k=k)              # low rank approximation
+term_by_document = lil_matrix(U.dot(diag(S)).dot(V))
+print "Full time: " + (time.time() - start_time)*1000
+
+# 7
+start_time = time.time()
+print "Normalizing each text-vector to one"
+for i in range(T):                                  # normalization - BOW-vector for each text has length 1
+    vect = term_by_document[i, :]
+    length = vect.dot(vect.transpose())[0, 0]
+    term_by_document[i, :] = numpy.multiply(vect, 1/length)
+    update_progress(i, T, time.time() - start_time)
+
+#########################################
+
+# 6
+print "Wyszukiwanie"
+query_vector = lil_matrix((N, 1))
+for word in prepare(query):                         # preparing query vector
+    query_vector[bag_of_words[word], 0] += 1
+
+try:
+    for i in range(N):                                  # query vector IDF normalization
+        query_vector[i, 0] *= idf[i]
+except KeyError:
+    pass
+
+query_length = query_vector.getnnz()                # query vector normalization
+if query_vector == 0:
+    raise KeyError('No of key words in bag-of-words')
+
+query_vector = numpy.multiply(query_vector, 1/query_length)
+
+result = query_vector.transpose().dot(term_by_document)     # ask Google
+
+print "Wyniki:"
+data = result.data
+max = sorted(data)[(text_count-k):]
+for i in range(text_count):
+    if result[0, i] in max:
+        print i
+print "Total: " + (time.time() - time)*1000
